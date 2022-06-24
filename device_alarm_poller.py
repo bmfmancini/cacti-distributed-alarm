eef21648e8aeeec656871a953d812f329b560f8a
bmfmancini <sean@seanmancini.com>
2022-06-23 21:09:17 -0400

    inital

--- device_alarm_poller.py
+++ device_alarm_poller.py
@@ -1,11 +1,5 @@
 #!/bin/python3
 
-###written by: Sean Mancini
-###Version: 3.0
-###purpose: This script acts as a outof band poller for Overwatch to distribute
-##alarm checking to each poller rather than only alarming via the primary poller
-
-
 import mysql.connector
 import sys
 import os
@@ -20,22 +14,33 @@ now = datetime.now()
 ###update config_poller_id with the id of the poller this script is running on this is used to find the status of the poller
 config_poller_id = "1"
 
-
 #### Notification settings ###
 
 ###Script execution ###
 execute_script = "n"
 path_to_script = ""
 
+## You can pass the following variables to scripts 
+#device_name           
+#device_hostname           
+#device_id
+## For down events you can pass fail_reason  
+##i.e "/tmp/notify_script device_name device_hostname"
+
+
+### emil notification settings ###
+email_enabled = "n"
+admin_email=""
+
+##slack notification settings (Not tested yet) ###
+slack_enabled = "n"
+slack_webhook_url=""
+
 
 ##syslog logging settings ###
-syslog_enabled = "n"
-syslog_server=""
-syslog_program=""
-
-###admin email settings ###
-admin_email_enabled = "n"
-admin_email = ""
+syslog_enabled = "y"
+syslog_server="127.0.0.1"
+syslog_program="alarm_poller"
 
 
 ### end notification settings ###
@@ -52,7 +57,7 @@ if not os.path.exists('.env'):
     print('could not find .env file')
     print('please create a .env file with the following contents:')
     print('DB_USER=<your_db_user>')
-    print('DB_PASSWORD=<mysql_password>')
+    print('DB_PASSW=<mysql_password>')
     print('DB=<database_name>')
     sys.exit()
 
@@ -73,8 +78,6 @@ except mysql.connector.Error as err:
 
 
 
-
-
 ##create cursor
 mycursor = mydb.cursor()
 
@@ -89,7 +92,7 @@ str(poller_interveral)
 ###based on the poller interval we adjust our checks 2 mins for 1 min and 10 mins for 5 mins in the sql statement
 
 if poller_interveral[0] == "60":
-    time_interveral = "3"
+    time_interveral = "2"
 elif poller_interveral[0] == "300":
     time_interveral = "10"
 
@@ -113,6 +116,8 @@ log_file_path = mycursor.fetchone()
 str(log_file_path)
 
 
+
+
 ###Sanity Checks###
 
 if execute_script == "y":
@@ -127,33 +132,43 @@ if execute_script == "y":
 
 if execute_script == "y":
     if path_to_script == "":
-        print("you have enabled script execution but have not specified a script path ")
+        print("you have enabled script executtion but have not specified a script path ")
         sys.exit()
+
+if email_enabled == "y":
+    if admin_email == "":
+        print("you have enabled email notification but have not specified an email address ")
+        sys.exit()
+
+if slack_enabled == "y":
+    if slack_webhook_url == "":
+        print("you have enabled slack notification but have not specified a webhook url ")
+        sys.exit()
+
 
 if syslog_enabled == "y":
     if syslog_server == "":
         print("you have enabled syslog logging but have not specified a syslog server ")
         sys.exit()
 
+
 if poller_enabled[0] == "on":
-    print("Cacti Poller is disabled Alarm checking will not be performed")
+    print(str(now) + " " + "ERROR: Cacti Poller is disabled Alarm checking will not be performed")
     os.system("echo " +  str(now) + " " + "'ERROR: Cacti Poller is disabled Alarm checking will not be performed' >> " + log_file_path[0])
     sys.exit()
 
 if global_poller_disabled[0] == '':
-    print("Cacti global polling is disabled Alarm checking will not be perfromed")
+    print(str(now) + " " + "ERROR: Cacti global polling is disabled Alarm checking will not be perfromed")
     os.system("echo " +  str(now) + " " + "'ERROR: Cacti global polling is disabled Alarm checking will not be perfromed' >> " + log_file_path[0])
     sys.exit()
+
 
 
 ### End Sanity Checks ###
 
 
 
-
-
-
-##find down devices
+##find down devices 
 
 ###since the primary poller has the full host table we dont want the script on the primary poller pulling
 ##the same data as the remote pollers would so if the script is running on poller1 we limit our search to just devices
@@ -161,7 +176,7 @@ if global_poller_disabled[0] == '':
 
 if config_poller_id == "1":
  device_down = "select id,hostname,description,status,status_last_error from host where status = 1 and poller_id = 1 and disabled = '' and status_fail_date  > NOW() - INTERVAL " + time_interveral + " MINUTE"
-
+ 
 device_up = "select id,hostname,description,status from host where status = 3 and poller_id = 1 and disabled = '' and status_rec_date  > NOW() - INTERVAL " + time_interveral + " MINUTE"
 
 
@@ -180,44 +195,44 @@ down_data = mycursor.execute(device_down)
 down_data = mycursor.fetchall()
 
 
+###loop over dowwn and up devices
 
-##########################################DOWN DEVICE PROCESSING SECTION ##############################################
 
+###Down device processing
 for device in down_data:
+    ##print device is down to log_path file
+    with open(log_file_path[0] , 'a') as log_file:
+        log_file.write(" ERROR: Device Name: " + " " +  device[2] + "Device ID "  + str(device[0])  + " is DOWN\n")
 
     ###lets make the variables a bit easier to work with
     fail_reason = device[4]
     device_name = device[2]
     device_hostname = device[1]
     device_id = device[0]
-
-    ##print device is down to log_path file
-    with open(log_file_path[0] , 'a') as log_file:
-        log_file.write(str(now) + " " + " OW_Sentinel" + " " +  "ERROR: Device Name: " + " " +  device_name + " " + "Device IP "
-        + device_hostname + " " +  "Device ID "  + str(device_id)  + " is DOWN\n" + "Reason: " + fail_reason + "\n" + "Alarm sent my sentinel\n")
-
-
-    print("Device: " + device_name + " " + "IP Address" + device_hostname + " " +  " is down due to " + fail_reason )
+    
+    print("Device: " + device_name +  " " + " AT IP " + device_hostname + " " + " is down due to " + fail_reason )
 
 
     if execute_script == "y":
-        script_return_code = os.system("/var/www/html/cacti/cli/snow_api_lab --desc " + device_name +
-        " --host " + device_name + " --sev 1 --type OW_SENTINEL" )
-        print("Script return code: " + str(script_return_code))
+        os.system(path_to_script)
 
+    if email_enabled == "y":
+        os.system("echo ERROR: Device Name: " + " " +  device_name + "Device ID "  + str(device_id)  + " Is DOWN!" " | mail -s 'Device: " + device[2] + " is DOWN!' " + admin_email)
+
+
+    if slack_enabled == "y":
+        os.system("curl -X POST --data 'payload=%7B%22text%22%3A%22WARNING: Device Name: " + " " +  device[2] + "Device ID "  + str(device[0])  + " is DOWN!' " + slack_webhook_url)
 
     if syslog_enabled == "y":
-        os.system("echo ERROR: Device Name: " + " " +  device_name + "Device ID "  + str(device_id)  +
-         " IS DOWN" " | logger -n " + syslog_server +  " -p  3" + " -t " + syslog_program)
-
-################################### END DOWN DEVICE PROCESSING SECTION ##############################################
+        os.system("echo WARNING: Device Name: " + " " +  device_name + "Device ID "  + str(device_id)  + "Is Down" " | logger -n " + syslog_server +  "-p "  + syslog_priority )
 
 
 
 
-######################UP DEVICE PROCESSING SECTION #####################################################################
+
+###up device processing
 for device in up_data:
-
+    
     device_name = device[2]
     device_hostname = device[1]
     device_id = device[0]
@@ -229,30 +244,29 @@ for device in up_data:
     device_id = device[0]
 
 
-    ##print device is up  to log_path file
+    ##print device is down to log_path file
     with open(log_file_path[0] , 'a') as log_file:
-        log_file.write(str(now) + "OW_Sentinel " + " " + "WARNING: Device Name: " + " " +  device_name + " " + "Device IP" + device_hostname + " "
-        +  "Device ID "  + str(device_id)  + " Has Restored" + "Message sent my sentinel\n")
+        log_file.write(" WARNING: Device Name: " + " " +  device[2] + "Device ID "  + str(device[0])  + " Has Restored\n")
 
     if execute_script == "y":
-        ###return the return code of the script
-        script_return_code = os.system("/var/www/html/cacti/cli/snow_api_lab --desc " + device_name +
-        " --host " + device_name + " --sev 0 --type OW_SENTINEL" )
-        print("Script return code: " + str(script_return_code))
+        os.system(path_to_script)
+    
+    if email_enabled == "y":
+        os.system("echo WARNING: Device Name: " + " " +  device[2] + "Device ID "  + str(device[0])  + " Has Restored" " | mail -s 'Device: " + device[2] + " is up' " + admin_email)
 
-
+    if slack_enabled == "y":
+        os.system("curl -X POST --data 'payload=%7B%22text%22%3A%22WARNING: Device Name: " + " " +  device[2] + "Device ID "  + str(device[0])  + " Has Restored%22%7D' " + slack_webhook_url)
     if syslog_enabled == "y":
-        os.system("echo WARNING: Device Name: " + " " +  device_name + "Device ID "  + str(device_id)  +
-        " Has Restored" " | logger -n " + syslog_server +  " -p  4"  + " -t " + syslog_program)
+        os.system("echo WARNING: Device Name: " + " " +  device[2] + "Device ID "  + str(device[0])  + " Has Restored" " | logger -n " + syslog_server +  " -p 5 -t "  + syslog_pogram )
 
 
 ##write status to log_path file
 with open(log_file_path[0] , 'a') as log_file:
-    log_file.write(str(now) + " " + "STATS: OW_Sentinel  ran " +  "Down Devices " + str(alarm_count[0]) + "\n")
+    log_file.write(str(now) + " " +  "STATS: Alarm Poller ran  " +  "\n" )
 
-###################### ENDF UP DEVICE PROCESSING SECTION #########################
-
-
+if syslog_enabled == "y":
+    os.system("echo Alarm Poller ran at: " + str(now)  + " | logger -n " + syslog_server +  " -p 5 -t  "  + syslog_program )
+    
 
 ###accept arguments for debug
 parser = argparse.ArgumentParser()
@@ -260,39 +274,24 @@ parser.add_argument("-d", "--debug", help="debug mode", action="store_true")
 
 ##check if debug mode is enabled
 if parser.parse_args().debug:
-
+    
     print("\n")
-    print("##########Cacti Poller settings ###########")
     print("Poller is running in debug mode")
     print("Script is configured to run on poller id " + config_poller_id)
     print("Poller interval is: " + str(poller_interveral))
     print("\n")
 
-
-
-    print("##########SQL Queries  ###########")
     print("SQL query run for Down Device: " + device_down)
     print("\n")
     print("down_data: " + str(down_data))
     print("\n")
     print("\n")
+
     print("SQL query run for Up Device: " + device_up)
     print("\n")
-
     print("up_data: " + str(up_data))
-
-    print("############## Notification settings #######################")
-    if syslog_enabled == "y":
-        print("Syslog logging is enabled")
-        print("Syslog server is: " + syslog_server)
-        print("Syslog program is: " + syslog_program)
-
-    if execute_script == "y":
-        print("Script execution is enabled")
-
 
 
 
 #close connection
 mydb.close()
-
